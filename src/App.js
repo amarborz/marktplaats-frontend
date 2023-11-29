5a9a738c9789c530aea908cefd21296e2f6128dd
amarborz <amarborz>
2023-11-29 15:10:39 +0100

    configured initial files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css'
+import { Header } from './components/Header/Header'
 
 function App() {
+	const name = 'Amar'
+	const age = 27
+
 	return (
 		<div className="App">
 			<h1>Marktplaats</h1>
+			<Header naam={name} leeftijd={age} />
 		</div>
 	)
 }

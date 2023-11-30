85af7ffd43a0de06f855aadf9d74c93ae5d65793
amarborz <amarborz>
2023-11-30 16:48:52 +0100

    added product card

--- src/components/MainCarousel/MainCarousel.jsx
+++ src/components/MainCarousel/MainCarousel.jsx
@@ -1,22 +1,29 @@
 import React from 'react'
+
 import { Carousel, Container } from 'react-bootstrap'
+import './mainCarousel.css'
+
 import electronics from '../../utils/images/electronics.webp'
 import books from '../../utils/images/books.webp'
 import clothes from '../../utils/images/clothes.webp'
 
 const carouselData = [
-	{ name: 'Electronics', description: 'Text', image: electronics },
-	{ name: 'Books', description: 'Text', image: books },
-	{ name: 'Clothes', description: 'Text', image: clothes },
+	{
+		name: 'Electronics',
+		description: 'Buy the newest gadgets!',
+		image: electronics,
+	},
+	{ name: 'Books', description: 'Read the newest books!', image: books },
+	{ name: 'Clothes', description: 'Buy stylish clothes!', image: clothes },
 ]
 
 const MainCarousel = () => {
 	return (
 		<Container className="w-50 d-flex justify-content-center">
-			<Carousel data-bs-theme="white" className="mt-5">
+			<Carousel data-bs-theme="white" className="mt-5" indicators={false}>
 				{carouselData.map((data) => {
 					return (
-						<Carousel.Item>
+						<Carousel.Item key={data.name}>
 							<img
 								className="d-block w-100 rounded"
 								src={data.image}
--- src/pages/HomePagina.jsx
+++ src/pages/HomePagina.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import MainCarousel from '../components/MainCarousel/MainCarousel'
+import ProductCard from '../components/ProductCard/ProductCard'
 
 const HomePagina = () => {
 	return (
 		<div>
 			<MainCarousel />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
+			<ProductCard />
 		</div>
 	)
 }

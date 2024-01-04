ce7d93697f8912ecee810b739e2d219ab0a2c977
Martin <martin_de_v@hotmail.com>
2024-01-04 16:10:41 +0100

    cleaned up data fetching code

--- src/components/ProductsPageList/ProductsPageList.jsx
+++ src/components/ProductsPageList/ProductsPageList.jsx
@@ -15,46 +15,23 @@ const ProductsPageList = ({ searchName }) => {
 	console.log(products)
 
 	useEffect(() => {
-		// console.log(userId)
-		// Fetch user's shopping cart data
-		// console.log(localStorage.getItem("token"))
-		// console.log("Trying to access shopping cart data")
-		fetch(`${process.env.REACT_APP_PATH}api/shoppingcart/by_user/${userId}`, {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json',
-				Authorization: localStorage.getItem('token'),
-				userId: userId,
-			},
-		})
-			.then((res) => {
-				// console.log("res1: ", res)
-				// if (!res.ok) {
-				// 	throw new Error("response was not ok")
-				// }
-				return res.json()
-			})
-			.then((shoppingCartData) => {
-				// Fetch item data
-				// console.log(shoppingCartData)
-				fetch(
-					`${process.env.REACT_APP_PATH}api/item/by_shopping_cart/${shoppingCartData.id}`,
-					{
-						method: 'GET',
-						headers: {
-							'Content-Type': 'application/json',
-							Authorization: localStorage.getItem('token'),
-							userId: userId,
-						},
-					}
-				)
-					.then((res) => {
-						// console.log("res2: ", res)
-						return res.json()
-					})
-					.then((itemData) => {
-						// Fetch products data
-						fetch(`${process.env.REACT_APP_PATH}api${searchName}`, {
+		// get products
+		fetch(`${process.env.REACT_APP_PATH}api${searchName}`)
+			.then((res) => res.json())
+			.then((productData) => {
+				// get shoppingcart of user
+				fetch(`${process.env.REACT_APP_PATH}api/shoppingcart/by_user/${userId}`, {
+					method: 'GET',
+					headers: {
+						'Content-Type': 'application/json',
+						Authorization: localStorage.getItem('token'),
+						userId: userId,
+					},
+				})
+					.then((res) => res.json())
+					.then((shoppingCartData) => {
+						// get items in shoppingcart
+						fetch(`${process.env.REACT_APP_PATH}api/item/by_shopping_cart/${shoppingCartData.id}`, {
 							method: 'GET',
 							headers: {
 								'Content-Type': 'application/json',
@@ -62,11 +39,9 @@ const ProductsPageList = ({ searchName }) => {
 								userId: userId,
 							},
 						})
-							.then((res) => {
-								// console.log(res)
-								return res.json()
-							})
-							.then((productData) => {
+							.then((res) => res.json())
+							.then((itemData) => {
+								// find which products are already in the shoppingcart
 								const updatedProducts = productData.map((product) => {
 									let color = 'rgb(0, 183, 255)'
 									for (let i = 0; i < itemData.length; i++) {
@@ -76,30 +51,17 @@ const ProductsPageList = ({ searchName }) => {
 									}
 									return { ...product, color }
 								})
-
 								setProducts(updatedProducts)
 								setLoggedIn(true)
-								// console.log("loggedIn set to true: ", loggedIn)
 							})
+
 					})
-			})
-			.catch((error) => {
-				console.log('catching the error')
-				fetch(`${process.env.REACT_APP_PATH}api${searchName}`, {
-					method: 'GET',
-					headers: {
-						'Content-Type': 'application/json',
-						// 'Authorization': localStorage.getItem("token"),
-						// 'userId': userId,
-					},
-				})
-					.then((res) => res.json())
-					.then((productData) => {
+					.catch((error) => {
 						setProducts(productData)
 						setLoggedIn(false)
 					})
 			})
-		console.log('loggedIn: ', loggedIn)
+
 		setIsLoading(false)
 	}, [searchName, loggedIn, userId])
 

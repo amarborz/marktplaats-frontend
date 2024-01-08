4ecbf5c08189432a4c382fea7b7d28a9ed0ea021
amarborz <amarborz>
2024-01-08 15:34:57 +0100

    fixed carousel

--- src/components/MainCarousel/MainCarousel.jsx
+++ src/components/MainCarousel/MainCarousel.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 import { Carousel, Container } from 'react-bootstrap'
 import './mainCarousel.css'
 
-import electronics from '../../utils/images/electronics.png'
-import books from '../../utils/images/books.webp'
-import clothes from '../../utils/images/clothes.jpg'
-import sports from '../../utils/images/sport.jpg'
-import furniture from '../../utils/images/furniture.jpg'
-import games from '../../utils/images/games.png'
+import electronics from '../../utils/images/electronics1.png'
+import books from '../../utils/images/books1.webp'
+import clothes from '../../utils/images/clothes1.jpg'
+import sports from '../../utils/images/sport1.jpg'
+import furniture from '../../utils/images/furniture1.jpg'
+import games from '../../utils/images/games1.png'
 import { useNavigate } from 'react-router-dom'
 
 const carouselData = [
@@ -44,7 +44,7 @@ const MainCarousel = () => {
 								src={data.image}
 								alt={data.name}
 								style={{
-									height: 400,
+									height: 500,
 									objectFit: 'cover',
 									filter: 'brightness(50%)',
 									cursor: 'pointer',

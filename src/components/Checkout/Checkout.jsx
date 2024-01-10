8c2a07a8774759fbafa093f5c5daf54b24a01d96
amarborz <amarborz>
2024-01-10 16:51:17 +0100

    improved data input a little

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -7,11 +7,11 @@ import { Link } from 'react-router-dom'
 const Checkout = ({ cartItems }) => {
 	const [totalAmount, setTotalAmount] = useState(0)
 	const [totalPrice, setTotalPrice] = useState(0)
-	const [selectedDate, setSelectedDate] = useState(null);
+	const [selectedDate, setSelectedDate] = useState('')
 
 	const handleDateChange = (event) => {
-		setSelectedDate(event.target.value);
-	  };
+		setSelectedDate(event.target.value)
+	}
 
 	useEffect(() => {
 		const newTotalAmount = cartItems.reduce(
@@ -32,24 +32,22 @@ const Checkout = ({ cartItems }) => {
 			<h6>Number of items: {totalAmount}</h6>
 			<h6>Price of items: {totalPrice.toFixed(2)}$</h6>
 			<h6>Delivery costs: {totalPrice >= 20 ? 0 : 3.5}$</h6>
-			<h4 className='mt-4'>
+			<h4 className="mt-2">
 				Final costs:{' '}
 				{parseFloat(totalPrice.toFixed(2)) + (totalPrice >= 20 ? 0 : 3.5)}$
 			</h4>
 
-			<div>
-				<h1>Select a Date</h1>
-				<input
-					type="date"
-					value={selectedDate}
-					onChange={handleDateChange}
-				/>
-				{selectedDate && (
-					<p>You selected: {selectedDate}</p>
-				)}
+			<div className="d-flex justify-content-center flex-column">
+				<h6 className="mt-4">Select a date to have your order delivered:</h6>
+				<input type="date" value={selectedDate} onChange={handleDateChange} />
+				{selectedDate && <p>You selected: {selectedDate}</p>}
 			</div>
 
-			<Link to={`/payment?totalPrice=${totalPrice.toFixed(2)}&deliveryDate=${selectedDate}`}>
+			<Link
+				to={`/payment?totalPrice=${totalPrice.toFixed(
+					2
+				)}&deliveryDate=${selectedDate}`}
+			>
 				<button className="checkoutButton">Checkout</button>
 			</Link>
 		</div>
